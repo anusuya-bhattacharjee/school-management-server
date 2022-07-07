2fe90b92623e8a98c9bf33fdf6531021e93c1968
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-07-07 15:54:14 +0530

    db-connected

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-const express = require('express')
-const app = express();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-  })
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2uf9b.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1});
 
-  app.listen(port, () => {
+async function run(){
+    try {
+        await client.connect();
+        // const RegisterUserCollection = client.db('devbox321').collection('RegisterUsers');
+        console.log('db connected!!!!')
+        
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+app.listen(port, () => {
     console.log(`School Management listening on port ${port}`)
-  })
+})
