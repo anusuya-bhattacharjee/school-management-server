2357e56e749b762e988c952f1b3c14f18c399598
Anusuya Bhattacharya <rupsa9875594718@gmail.com>
2022-07-13 15:03:46 +0530

    user-profile-done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const multer = require('multer');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -7,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+// app.use(multer().single('image'));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2uf9b.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -15,10 +18,25 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+      if(err) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
+
 async function run() {
   try {
     await client.connect();
-    // const RegisterUserCollection = client.db('SchoolManagement').collection('RegisterUsers');
+    const registeredUserCollection = client.db('SchoolManagement').collection('regisUsers');
     const StudentCollention = client
       .db("SchoolManagement")
       .collection("Student");
@@ -26,16 +44,19 @@ async function run() {
 
     app.post("/addStudentDetails", async (req, res) => {
       const student = req.body;
+    //   const image = req.file;
+    //   console.log(image);
       const query = { email_address: student.email_address };
       const options = { upsert: true };
       const updatedDoc = {
         $set: {
           studentId: student.studentId,
-          fristName: student.firstName,
+          firstName: student.firstName,
           lastName: student.lastName,
           address: student.address,
           pincode: student.pincode,
           phone: student.phone,
+        //   image: student.image
         },
       };
       const result = await StudentCollention.updateOne(query, updatedDoc, options);
@@ -49,6 +70,33 @@ async function run() {
       const cursor = await StudentCollention.findOne(query);
       res.send(cursor);
     });
+
+    app.get('/allProfiles', async (req, res) => {
+        const query = {};
+        const cursor = StudentCollention.find(query);
+        const allProfiles = await cursor.toArray();
+        res.send(allProfiles);
+    })
+
+    app.get('/admin/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = await registeredUserCollection.findOne({email: email});
+        const isAdmin = user.role === "admin";
+        res.send({admin: isAdmin});
+    })
+
+    app.put('/user/:email', async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        const filter = { email: email };
+        const options = { upsert: true};
+        const updatedDoc = {
+          $set: user,
+        };
+        const result = await registeredUserCollection.updateOne(filter, updatedDoc, options);
+        const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'})
+        res.send({result, token});
+      })
   } 
   finally {
   }
